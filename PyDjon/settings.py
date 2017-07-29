fd4a06ed8abdd066962548e3b48cc3be43ef66a2
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-07-29 22:47:40 +0200

    blog created and ready for migration

--- PyDjon/settings.py
+++ PyDjon/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'myprofile',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- myprofile/views.py
+++ myprofile/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'myprofile/home.html')
+
+
+def contact(request):
+    return render(request, 'myprofile/basic.html', {
+        'content': ['To contact me drop me an email on:', 'email@email.com']
+    })
